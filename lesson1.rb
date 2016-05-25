8b5ff122149b1ebce525faad10e43ed3aa519b4e
walkman777 <gonor.alexandr@gmailcom>
2016-05-25 21:05:19 +0300

    lesson1

---
+++ lesson1.rb
@@ -0,0 +1,11 @@
+generated_number = Array.new(4) { Array.new(4) {rand(-10...10) } }
+sum = 0
+generated_number.each do |row|
+  row.each do |item|
+    if item < 0
+      sum += item      
+    end
+    puts item if item < 0
+  end
+end
+puts 'sum' => sum
